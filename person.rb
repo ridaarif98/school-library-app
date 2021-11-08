79336dc8de29b190c473a1aad03dcd44e16c1983
ridaarif98 <ridaarif16@gmail.com>
2021-11-08 14:56:38 +0100

    Create teacher class and inherir from person. Fix linter errors

--- person.rb
+++ person.rb
@@ -1,30 +1,21 @@
 class Person
-	attr_reader :id
-	attr_accessor :name, :age
+  attr_reader :id
+  attr_accessor :name, :age
 
-	def initialize(name ="Unknown", age, parent_permission: true)
-		@id = Random.rand(1..1000)
-		@name = name
-		@age = age
-		@parent_permission = parent_permission
-	end
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
 
-	def can_use_survive
-		if age >= 18 || parent_permission == true
-			true
-		else
-			false
-		end
-	end
+  def can_use_survive
+    age >= 18 || parent_permission == true
+  end
 
-	private 
-	def is_of_age
-		@age >= 18
-	end
+  private
 
+  def of_age
+    @age >= 18
+  end
 end
-
-person = Person.new("Rida", 22,)
-puts(person.name)
-person.name = "Ali"
-puts(person.name)
--- student.rb
+++ student.rb
@@ -1,14 +1,12 @@
-require "./person.rb"
+require './person'
 
 class Student < Person
+  def initialize(classroom, age, name = 'Unknown', parent_permission: true)
+    super(name, age, parent_permission)
+    @classroom = classroom
+  end
 
-  def initialize(name ="Unknown", age, parent_permission: true, classroom)
-		super(name, age, parent_permission)
-		@classroom: classroom
-	end
-
-	def play_hooky
-		"¯\(ツ)/¯"
-	end
-    
+  def play_hooky
+    "¯\(ツ)/¯"
+  end
 end
