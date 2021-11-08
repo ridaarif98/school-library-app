5c8ef44ee98a1595ebb4ec99a71d74794e15d239
ridaarif98 <ridaarif16@gmail.com>
2021-11-08 14:31:29 +0100

    Create methods for person class

--- person.rb
+++ person.rb
@@ -1,29 +1,28 @@
 class Person
+	attr_reader :id
+	attr_accessor :name, :age
 
-	def initialize(name ="Unknown", age, parent_permission = true)
+	def initialize(name ="Unknown", age, parent_permission: true)
 		@id = Random.rand(1..1000)
 		@name = name
 		@age = age
 		@parent_permission = parent_permission
 	end
 
-	def id
-		@id
+	def can_use_survive
+		if age >= 18 || parent_permission == true
+			true
+		else
+			false
+		end
 	end
 
-	def name
-		@name
+	private 
+	def is_of_age
+		@age >= 18
 	end
 
-	def age
-		@age
-	end
-
-	def name=(value)
-		@name = value
-	end
-
-	def age=(value)
-		@age = value
-	end
 end
+
+person = Person.new("Rida", 22,)
+puts(person.can_use_survive)
