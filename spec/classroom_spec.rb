80a832b09fc2c94da43bedd737f95c87f4275826
Adedamola Shittu <shittuadedamola@rocketmail.com>
2021-11-17 15:10:17 +0100

    Add Tests For Rentals Class And Update classroom_spec.rb FIle

--- spec/classroom_spec.rb
+++ spec/classroom_spec.rb
@@ -2,21 +2,28 @@ require_relative '../classroom'
 require_relative  '../student'
 
 describe Classroom do
-    before(:each) do 
+    before(:each) do
         @classroom = Classroom.new('Chemistry')
-        @stu  = Student.new( age: 19, classroom: @classroom.label, name: 'Ali', parent_permission: false ) 
-    end
+        @stu  = Student.new( age: 19, classroom: @classroom.label, name: 'Ali', parent_permission: false )
+      end
 
-    it " show instance of classroom class" do
+      it " show instance of classroom class" do
         expect(@classroom).to be_instance_of Classroom
-    end
+      end
 
-    it "add student details to student array" do
+      it "add student details to student array" do
         @classroom.add_student(@stu)
         expect(@classroom.students).to include @stu
-    end
+      end
 
-    it "check student classroom match with class label" do
+      it "check student classroom match with class label" do
         expect(@classroom.label).to eql @stu.classroom
+      end
+
+      it "checks the size of students array" do
+        @stu1  = Student.new( age: 29, classroom: @classroom.label, name: 'Dammy', parent_permission: true )
+        @classroom.add_student(@stu)
+        @classroom.add_student(@stu1)
+        expect(@classroom.students.size).to be 2
     end
 end
---
+++ spec/rentals_spec.rb
@@ -0,0 +1,48 @@
+require_relative '../rental'
+require_relative '../teacher'
+require_relative '../student'
+require_relative '../book'
+require_relative '../classroom'
+
+describe Rental do
+  before(:each) do
+    @book = Book.new(title: 'Fairy Tales', author: 'Rida')
+    @classroom = Classroom.new('Chemistry')
+    @stu  = Student.new( age: 19, classroom: @classroom.label, name: 'Ali', parent_permission: false )
+    @teacher  = Teacher.new( age: 45, specialization: 'Maths', name: 'Mr Rajesh')
+    @rental1 = Rental.new('2021/12/12', @stu, @book)
+    @rental2 = Rental.new('2021/12/27', @teacher, @book)
+  end
+
+  it "show instance of rental class" do
+    expect(@rental1).to be_instance_of Rental
+  end
+
+  it "verifies rental date for student" do
+    expect(@rental1.date).to match '2021/12/12'
+  end
+
+  it "verifies class of person(Student) who created a rental" do
+    expect(@rental1.person).to be_instance_of Student
+  end
+
+  it "verifies rental date for teacher" do
+    expect(@rental2.date).not_to match '2011/12/12'
+  end
+
+  it "verifies class of person(Teacher) who created a rental" do
+    expect(@rental2.person).to be_instance_of Teacher
+  end
+
+  it "expects students to be in rental array" do
+    expect(@stu.rentals).to include @rental1
+  end
+
+  it "expects teacher to be in rental array" do
+    expect(@teacher.rentals).to include @rental2
+  end
+
+  it "expects book to be in rental array" do
+    expect(@book.rentals.size).to be 2
+  end
+end
