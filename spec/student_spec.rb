3a2c7cf1cd43dc254b078c93425dbc6e6062dc4d
ridaarif98 <ridaarif16@gmail.com>
2021-11-17 13:42:42 +0100

    Write tests for student class

---
+++ spec/student_spec.rb
@@ -0,0 +1,26 @@
+require_relative '../student'
+require_relative '../classroom'
+require_relative '../person'
+
+describe Student do
+    before(:each) do 
+        @classroom = Classroom.new('Chemistry')
+        @stu  = Student.new( age: 19, classroom: @classroom.label, name: 'Ali', parent_permission: false ) 
+     end 
+    
+    it " show instance of student class" do
+        expect(@stu).to be_instance_of Student
+    end
+
+    it "show inheritance from person class" do
+        expect(@stu).to be_kind_of Person
+    end
+
+    it "check play_hooky not to be correct" do
+        expect(@stu.play_hooky).not_to match'/()/'
+    end
+
+    it "match classroom value" do
+        expect(@stu.classroom).to eql @classroom.label 
+    end
+end
